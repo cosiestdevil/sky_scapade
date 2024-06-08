c6e55721ba27c24f18b3f7460fc61e8e72da47c8
Justin Wiblin <justinw@cosiestdevil.uk>
2024-06-08 10:03:01 +0100

    Refactor Discord client handling
    The Discord client is now wrapped in an Option for better error handling. The connection process has been updated to handle potential errors, logging them instead of unwrapping. Similarly, the activity update function now checks if the client exists before attempting to set activity. This should improve stability and provide clearer error messages when issues arise with the Discord integration.

--- src/discord.rs
+++ src/discord.rs
@@ -2,38 +2,53 @@ use bevy::prelude::*;
 use discord_rich_presence::{activity::{self, Timestamps}, DiscordIpc, DiscordIpcClient};
 pub struct DiscordPlugin;
 #[derive(Resource, Deref, DerefMut)]
-pub struct DiscordClient(DiscordIpcClient);
+pub struct DiscordClient(Option<DiscordIpcClient>);
 
 impl Plugin for DiscordPlugin {
     fn build(&self, app: &mut App) {
         let client = DiscordIpcClient::new(include_str!("DISCORD_CLIENT.ID")).unwrap();
         app.init_resource::<ActivityState>();
-        app.insert_resource(DiscordClient(client));
+        app.insert_resource(DiscordClient(Some(client)));
         app.add_systems(Startup, startup_client);
         app.add_systems(FixedUpdate, check_activity_changed);
     }
 }
 fn startup_client(mut client: ResMut<DiscordClient>) {
-    client.connect().unwrap();
+    match &mut client.0{
+        Some(discord) =>{
+            if let Err(err) = discord.connect(){
+                error!("{}",err);
+                client.0 = None;
+            }
+        },
+        None => {},
+    }
+    
 }
 fn check_activity_changed(activity: Res<ActivityState>, mut client: ResMut<DiscordClient>) {
-    if activity.is_changed() {
-        let mut discord_activity: activity::Activity = activity::Activity::new();
-        if let Some(state) = &activity.state {
-            discord_activity = discord_activity.state(state);
-        }
-        if let Some(details) = &activity.details {
-            discord_activity = discord_activity.details(details);
-        }
-        if let Some(start) = &activity.start{
-            discord_activity = discord_activity.timestamps(Timestamps::new().start(*start));
-        }
-        let res = client.set_activity(discord_activity);
-
-        if let Err(why) = res {
-            error!("Failed to set presence: {}", why);
-        }
+    match &mut client.0{
+        Some(client) => {
+            if activity.is_changed() {
+                let mut discord_activity: activity::Activity = activity::Activity::new();
+                if let Some(state) = &activity.state {
+                    discord_activity = discord_activity.state(state);
+                }
+                if let Some(details) = &activity.details {
+                    discord_activity = discord_activity.details(details);
+                }
+                if let Some(start) = &activity.start{
+                    discord_activity = discord_activity.timestamps(Timestamps::new().start(*start));
+                }
+                let res = client.set_activity(discord_activity);
+        
+                if let Err(why) = res {
+                    error!("Failed to set presence: {}", why);
+                }
+            }
+        },
+        None => {},
     }
+    
 }
 #[derive(Debug, Resource, Default, Clone)]
 pub struct ActivityState {
